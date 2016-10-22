e3f72a1fb04dce2398f3276771e46074f5ed73d7
John Franklin <ionath@hotmail.co.uk>
2016-10-22 22:04:18 +0100

    Added code to NetworkSession.

--- NetworkSession.cpp
+++ NetworkSession.cpp
@@ -13,12 +13,101 @@
 
 #include "NetworkSession.h"
 
-NetworkSession::NetworkSession() {
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <cassert>
+
+NetworkSession::NetworkSession()
+{
 }
 
-NetworkSession::NetworkSession(const NetworkSession& orig) {
+NetworkSession::NetworkSession(int socketFD)
+{
+	m_clientSocketFD = socketFD;
 }
 
-NetworkSession::~NetworkSession() {
+NetworkSession::NetworkSession(NetworkSession&& orig)
+{
+	*this = orig;
 }
 
+NetworkSession::~NetworkSession() 
+{
+	closeConnection();
+}
+
+NetworkSession& NetworkSession::operator=(NetworkSession&& orig)
+{
+	m_clientSocketFD = orig.m_clientSocketFD;
+}
+
+NetworkSession::NetworkSession(const NetworkSession& orig)
+{
+	assert(0);
+}
+
+NetworkSession& NetworkSession::operator=(const NetworkSession& orig)
+{
+	assert(0);
+}
+
+bool NetworkSession::isValidSocket()
+{
+	return m_clientSocketFD != INVALID_SOCKET;
+}
+
+std::string NetworkSession::readFromConnection()
+{
+	const size_t BUFFER_SIZE = 256;
+	char buffer[BUFFER_SIZE];
+	bzero(buffer,BUFFER_SIZE);
+	int n = read(m_clientSocketFD, buffer, BUFFER_SIZE-1);
+	if (n == 0)
+	{
+		// Connection closed
+		connectionClosedByClient();
+	}
+	if (n < 0)
+	{
+		error("ERROR reading from socket");
+	}
+	
+	return std::string(buffer);
+}
+
+void NetworkSession::writeToConnection(const std::string& data)
+{
+	int n = write(m_clientSocketFD, data.c_str(), data.length());
+	if (n == 0)
+	{
+		// Connection closed
+		connectionClosedByClient();
+	}
+	if (n < 0)
+	{
+		error("ERROR writing to socket");
+	}
+}
+
+void NetworkSession::closeConnection()
+{
+	if (isValidSocket())
+	{
+		close(m_clientSocketFD);
+		m_clientSocketFD = INVALID_SOCKET;
+	}
+}
+
+void NetworkSession::error(const char *msg)
+{
+	
+}
+void NetworkSession::connectionClosedByClient()
+{
+	
+}
--- NetworkSession.h
+++ NetworkSession.h
@@ -14,13 +14,41 @@
 #ifndef NETWORKSESSION_H
 #define NETWORKSESSION_H
 
-class NetworkSession {
+#include <string>
+
+const int INVALID_SOCKET = -1;
+
+class NetworkSession
+{
 public:
 	NetworkSession();
-	NetworkSession(const NetworkSession& orig);
+	NetworkSession(int socketFD);
+	NetworkSession(NetworkSession&& orig);
 	virtual ~NetworkSession();
+	
+	NetworkSession& operator=(NetworkSession&& orig);
+	
+	// Do not allow copying. socket FD should only be stored in one place
 private:
-
+	NetworkSession(const NetworkSession& orig);
+	NetworkSession& operator=(const NetworkSession& orig);
+	
+	
+public:
+	
+	bool isValidSocket();
+	
+	std::string readFromConnection();
+	void writeToConnection(const std::string& data);
+	
+	void closeConnection();
+	
+private:
+	int m_clientSocketFD = INVALID_SOCKET;
+	
+private:
+	void error(const char *msg);
+	void connectionClosedByClient();
 };
 
 #endif /* NETWORKSESSION_H */
